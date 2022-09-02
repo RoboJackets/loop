c22ebac5a30bcd4d2f9d9860238c61dcef701360
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-09-02 12:34:06 -0400

    Cleaning up unneeded starter code

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,6 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
     ];
 
     /**
@@ -29,8 +26,6 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
-        'remember_token',
     ];
 
     /**
@@ -39,6 +34,5 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }
