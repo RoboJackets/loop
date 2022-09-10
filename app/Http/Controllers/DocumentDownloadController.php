1fb3cdffb52362e1d0a8ad28a7d14d804f872293
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-09-10 15:53:06 -0400

    Switch to using Storage facade

--- app/Http/Controllers/DocumentDownloadController.php
+++ app/Http/Controllers/DocumentDownloadController.php
@@ -5,15 +5,16 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Models\DocuSignEnvelope;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class DocumentDownloadController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(DocuSignEnvelope $envelope): BinaryFileResponse
+    public function __invoke(DocuSignEnvelope $envelope): StreamedResponse
     {
-        return response()->download($envelope->sofo_form_filename);
+        return Storage::download($envelope->sofo_form_filename);
     }
 }
