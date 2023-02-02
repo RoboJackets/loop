e52b3624dabdcd8e99e96fc73e16f7519fbfdc0a
Kristaps Berzinch <kristapsberzinch@gmail.com>
2023-02-01 19:56:03 -0500

    Ignore Phan finding

--- app/Nova/ExpenseReport.php
+++ app/Nova/ExpenseReport.php
@@ -77,6 +77,7 @@ class ExpenseReport extends Resource
 
             Badge::make('Status')
                 ->map([
+                    // @phan-suppress-next-line PhanTypeInvalidArrayKeyLiteral
                     null => 'danger',
                     'Draft' => 'info',
                     'In Progress' => 'info',
