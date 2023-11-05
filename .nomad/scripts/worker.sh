4c5e047bcf9e25e8d6b7023f497cdf6d5acaa9e5
Kristaps Berzinch <kristapsberzinch@gmail.com>
2023-11-05 15:32:39 -0500

    Create missing directory

--- .nomad/scripts/worker.sh
+++ .nomad/scripts/worker.sh
@@ -1,4 +1,5 @@
 mkdir --parents /assets/${NOMAD_JOB_NAME}/storage/thumbnail/
+mkdir --parents /app/storage/app/public/thumbnails/
 ln --symbolic /assets/${NOMAD_JOB_NAME}/storage/thumbnail/ /app/storage/app/public/thumbnails/
 php artisan config:cache --no-interaction --verbose
 php artisan view:cache --no-interaction --verbose
