fe7ac31dd32722108e0fbe2d7ecc8d4fafe05e3f
Kristaps Berzinch <kristapsberzinch@gmail.com>
2023-11-05 15:39:58 -0500

    derp

--- .nomad/scripts/worker.sh
+++ .nomad/scripts/worker.sh
@@ -1,6 +1,6 @@
 mkdir --parents /assets/${NOMAD_JOB_NAME}/storage/thumbnail/
-mkdir --parents /app/storage/app/public/thumbnails/
-ln --symbolic /assets/${NOMAD_JOB_NAME}/storage/thumbnail/ /app/storage/app/public/thumbnails/
+mkdir --parents /app/storage/app/public/thumbnail/
+ln --symbolic /assets/${NOMAD_JOB_NAME}/storage/thumbnail/ /app/storage/app/public/thumbnail/
 php artisan config:cache --no-interaction --verbose
 php artisan view:cache --no-interaction --verbose
 php artisan event:cache --no-interaction --verbose
