86b8bb3fbbcd5443fd4dc78b70af1a6d7219d24c
Kristaps Berzinch <kristapsberzinch@gmail.com>
2023-11-05 18:57:17 -0500

    Disable default timestamps on DataSource

--- app/Models/DataSource.php
+++ app/Models/DataSource.php
@@ -9,6 +9,13 @@ use Illuminate\Database\Eloquent\Model;
 class DataSource extends Model
 {
     /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
      * The attributes that should be cast to native types.
      *
      * @var array<string,string>
