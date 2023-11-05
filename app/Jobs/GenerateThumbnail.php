e9b6dd1e47775187184227a4b9d92d033f9f9f4d
Kristaps Berzinch <kristapsberzinch@gmail.com>
2023-11-05 16:39:47 -0500

    Add single quotes around file paths for exec

--- app/Jobs/GenerateThumbnail.php
+++ app/Jobs/GenerateThumbnail.php
@@ -39,7 +39,7 @@ class GenerateThumbnail implements ShouldBeUnique, ShouldQueue
             return;
         }
 
-        $command = 'file --mime-type -b '.escapeshellarg($this->pdf_path);
+        $command = 'file --mime-type -b \''.escapeshellarg($this->pdf_path).'\'';
         $output = [];
         $exitCode = -1;
 
@@ -58,8 +58,8 @@ class GenerateThumbnail implements ShouldBeUnique, ShouldQueue
         }
 
         // Renders PDF to 266px wide, crops out 5 pixels from left, top, and right, resulting in 256px wide image
-        $command = 'pdftocairo -png -singlefile -scale-to-x 266 -scale-to-y -1 -x 5 -y 5 -W 256 '.
-            escapeshellarg($this->pdf_path).' '.$thumbnail_path;
+        $command = 'pdftocairo -png -singlefile -scale-to-x 266 -scale-to-y -1 -x 5 -y 5 -W 256 \''.
+            escapeshellarg($this->pdf_path).'\' \''.$thumbnail_path.'\'';
         $output = [];
         $exitCode = -1;
 
