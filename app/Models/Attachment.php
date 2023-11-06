55ce980ef31b92bc384f0c65207ba0d153cc438f
Kristaps Berzinch <kristapsberzinch@gmail.com>
2023-11-05 20:47:46 -0500

    Handle files with uppercase JPG extension

--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -164,7 +164,7 @@ class Attachment extends Model
             return null;
         }
 
-        $extension = str_ends_with($full_file_path, '.jpg') ? '.jpg' : '.png';
+        $extension = str_ends_with(strtolower($full_file_path), '.jpg') ? '.jpg' : '.png';
 
         $thumbnail_relative_path = '/thumbnail/'.hash_file('sha512', $full_file_path).$extension;
 
