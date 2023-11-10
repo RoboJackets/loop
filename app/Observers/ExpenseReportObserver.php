5f181a68000bbfa3e7dfbc10c23b1b5525f869c9
Kristaps Berzinch <kristapsberzinch@gmail.com>
2023-11-10 15:07:25 -0500

    Run MatchExpenseReport if not matched to an Engage request

--- app/Observers/ExpenseReportObserver.php
+++ app/Observers/ExpenseReportObserver.php
@@ -6,13 +6,17 @@ namespace App\Observers;
 
 use App\Jobs\MatchExpenseReport;
 use App\Models\DocuSignEnvelope;
+use App\Models\EngagePurchaseRequest;
 use App\Models\ExpenseReport;
 
 class ExpenseReportObserver
 {
     public function saved(ExpenseReport $expenseReport): void
     {
-        if (DocuSignEnvelope::whereExpenseReportId($expenseReport->id)->doesntExist()) {
+        if (
+            DocuSignEnvelope::whereExpenseReportId($expenseReport->id)->doesntExist() &&
+            EngagePurchaseRequest::whereExpenseReportId($expenseReport->id)->doesntExist()
+        ) {
             MatchExpenseReport::dispatch($expenseReport);
         }
     }
