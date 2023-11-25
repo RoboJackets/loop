0c88bcb54fecbcb03614e78d924989c0d7dffce3
Kristaps Berzinch <kristapsberzinch@gmail.com>
2023-11-25 15:28:48 -0600

    Tweak email template

--- resources/views/mail/emailrequestprocessed.blade.php
+++ resources/views/mail/emailrequestprocessed.blade.php
@@ -1,11 +1,11 @@
-Loop completed processing email request {{ $email->id }} sent at {{ $email->email_sent_at }}.
+Loop completed processing email request #{{ $email->id }} sent at {{ $email->email_sent_at }}.
 
 The following information was extracted:
 
 Vendor: {{ $email->vendor_name }}
-Amount: {{ $email->vendor_document_amount }}
+Amount: ${{ $email->vendor_document_amount }}
 Reference number: {{ $email->vendor_document_reference }}
-Document date: {{ $email->vendor_document_date }}
+Document date: {{ $email->vendor_document_date?->format('Y-m-d') }}
 
 @forelse ($validation_errors as $error)
 @if($loop->first)
