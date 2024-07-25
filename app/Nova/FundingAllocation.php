e4534c4f1e5833882e8a07145d58ddb196daaf17
Kristaps Berzinch <kristapsberzinch@gmail.com>
2024-07-24 21:04:54 -0400

    Eager-load fiscal year on FundingAllocation index

--- app/Nova/FundingAllocation.php
+++ app/Nova/FundingAllocation.php
@@ -57,6 +57,15 @@ class FundingAllocation extends Resource
     public static $group = 'SOFO';
 
     /**
+     * The relationships that should be eager loaded on index queries.
+     *
+     * @var array<string>
+     */
+    public static $with = [
+        'fiscalYear',
+    ];
+
+    /**
      * Get the fields displayed by the resource.
      */
     public function fields(NovaRequest $request): array
