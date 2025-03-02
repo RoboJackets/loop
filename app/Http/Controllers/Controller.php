b613ff85d1c90abf0d3a2b99d5ae087819f6d9ce
Shift <shift@laravelshift.com>
2025-03-02 16:11:37 +0000

    Remove base controller inheritance

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use Illuminate\Routing\Controller as BaseController;
 
 /**
  * Base controller with traits included already.
  *
  * @phan-suppress PhanUnreferencedClass
  */
-abstract class Controller extends BaseController
+abstract class Controller
 {
 }
