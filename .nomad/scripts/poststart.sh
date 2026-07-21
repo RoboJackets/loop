f09fc834d446d23f1b6a9bc7b53fd9ff088eb68d
Kristaps Berzinch <kristapsberzinch@gmail.com>
2026-07-21 17:24:25 -0400

    Index high-value models at startup

--- .nomad/scripts/poststart.sh
+++ .nomad/scripts/poststart.sh
@@ -1,1 +1,8 @@
 php artisan scout:sync-index-settings --no-interaction --verbose
+php artisan scout:import BankTransaction --no-interaction --verbose
+php artisan scout:import EmailRequest --no-interaction --verbose
+php artisan scout:import EngagePurchaseRequest --no-interaction --verbose
+php artisan scout:import ExpenseReport --no-interaction --verbose
+php artisan scout:import ExpenseReportLine --no-interaction --verbose
+php artisan scout:import ExternalCommitteeMember --no-interaction --verbose
+php artisan scout:import User --no-interaction --verbose
