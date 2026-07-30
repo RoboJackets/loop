9d62b474044fa3fed083f7bf8c8c7a40d11d4c6a
Kristaps Berzinch <kristapsberzinch@gmail.com>
2026-07-30 14:48:30 -0400

    Remove vestigial ping command and prestart junk

--- .nomad/scripts/prestart.sh
+++ .nomad/scripts/prestart.sh
@@ -4,21 +4,7 @@ php artisan event:cache --no-interaction --verbose
 php artisan route:cache --no-interaction --verbose
 php artisan cache:clear --no-interaction --verbose
 php artisan migrate --no-interaction --force --verbose
-
-if [ ${APP_ENV} = "production" ]
-then
-    export SKIP_DEPENDENCY_ANALYZER=true
-fi
-
-if [ ${APP_ENV} != "google-play-review" ]
-then
-    export SKIP_PHPSTAN_CHECKS=true
-    if ! php artisan ping --no-interaction --verbose
-    then
-        export SKIP_HTTP_CHECKS=true
-    fi
-    php artisan config:cache --no-interaction --verbose
-fi
+php artisan config:cache --no-interaction --verbose
 
 mkdir --parents /assets/${NOMAD_JOB_NAME}/
 cp --recursive --verbose public/* /assets/${NOMAD_JOB_NAME}/
